abbcb899d220fe412958954c9a50bb3dd6af2fba
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-02-22 15:18:22 +0530

    Build

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Link, NavLink, Switch,Route } from "react-router-dom";
+import React from "react";
+import { Link, NavLink, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import PostDetail from "./PostDetail";
 import PostList from "./PostList";
 
-
 export default function Blog() {
-    return (
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          {/* Create NavLink to About and Posts */}
-        </nav>
-  
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {/* Create Routes to About, PostList and PostDetail */}
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/posts">Posts</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/posts" component={PostList} />
+        <Route path="/posts/:id" component={PostDetail} />
+      </Switch>
+    </div>
+  );
+}
