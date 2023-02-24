901eb351d0d0b7fe18a26adb62c2fd638ef9be8c
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-02-24 17:37:44 +0530

    Update PostDetail.js

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 
-export default function PostDetail({match}) {
+export default function PostDetail() {
   const {id} = useParams();
   return <h1>Post Detail: {id}</h1>;
 }
